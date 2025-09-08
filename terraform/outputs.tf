8c2a19d0375568b48563becb22078bcc107e9303
Glauber-Developer <T0007222@randon.com.br>
2025-09-08 09:25:54 -0300

    chore: outputs changes

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,8 @@
 output "app_instance_public_ip" {
   description = "IP público da instância EC2 da aplicação"
   value       = aws_eip.pointtils_eip.public_ip
+  # Garantir que o output é uma string para evitar erros de formatação
+  depends_on  = [aws_eip.pointtils_eip]
 }
 
 output "app_instance_public_dns" {
@@ -11,6 +13,7 @@ output "app_instance_public_dns" {
 output "s3_bucket_name" {
   description = "Nome do bucket S3 para testes de API"
   value       = aws_s3_bucket.pointtils_api_tests.bucket
+  depends_on  = [aws_s3_bucket.pointtils_api_tests]
 }
 
 output "ecr_repository_url" {
