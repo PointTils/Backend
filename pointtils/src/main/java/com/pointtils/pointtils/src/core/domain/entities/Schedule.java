f45de25bc3be2331243940f042cecbae11cf86aa
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-09-19 20:18:51 -0300

    refactor: refactor to copy #US17 branch iteration

--- pointtils/src/main/java/com/pointtils/pointtils/src/core/domain/entities/Schedule.java
+++ pointtils/src/main/java/com/pointtils/pointtils/src/core/domain/entities/Schedule.java
@@ -1,46 +1,39 @@
 package com.pointtils.pointtils.src.core.domain.entities;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalTime;
 import java.util.UUID;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.pointtils.pointtils.src.core.domain.entities.enums.DayOfWeek;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 @Entity
 @Table(name = "schedule")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@ToString
 public class Schedule {
-    
+
     @Id
-    @GeneratedValue
-    @Column(columnDefinition = "uuid")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "interpreter_id", nullable = false)
-    private Interpreter interpreter;
+    @Column(name = "interpreter_id", nullable = false)
+    private UUID interpreterId;
 
-    private LocalDate day;
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(nullable = false)
+    private DayOfWeek day;
 
     @Column(name = "start_time", nullable = false)
     private LocalTime startTime;
 
     @Column(name = "end_time", nullable = false)
     private LocalTime endTime;
+
 }
---
+++ pointtils/src/main/java/com/pointtils/pointtils/src/core/domain/entities/enums/DayOfWeek.java
@@ -0,0 +1,11 @@
+package com.pointtils.pointtils.src.core.domain.entities.enums;
+
+public enum DayOfWeek {
+    MON,
+    TUE,
+    WEN,
+    THU,
+    FRI,
+    SAT,
+    SUN
+}
