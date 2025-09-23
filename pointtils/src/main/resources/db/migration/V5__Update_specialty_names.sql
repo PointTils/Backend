86898f46652b11bf921c2dd8563af9492267f69a
fernanda.mello <fernanda.mello02@edu.pucrs.br>
2025-09-22 21:04:21 -0300

    Corrige migration de especialidades para chumbar ids que serão utilizados pelo frontend

--- pointtils/src/main/resources/db/migration/V5__Update_specialty_names.sql
+++ pointtils/src/main/resources/db/migration/V5__Update_specialty_names.sql
@@ -1,18 +1,15 @@
 -- V5__Update_specialty_names.sql
 
 -- 1. Atualizar os registros existentes de especialidades para respeitar lista passada pelos stakeholders
-UPDATE specialties
-SET name = 'Intérprete de Libras'
-WHERE id = (SELECT id FROM specialties WHERE name = 'Libras' ORDER BY id LIMIT 1);
 
-UPDATE specialties
-SET name = 'Guia-intérprete de Libras'
-WHERE id = (SELECT id FROM specialties WHERE name = 'Tradução Simultânea' ORDER BY id LIMIT 1);
+DELETE FROM user_specialties
+WHERE specialtie_id IN (SELECT id FROM specialties WHERE name IN ('Libras', 'Tradução Simultânea'));
 
-UPDATE specialties
-SET name = 'Intérprete Tátil'
-WHERE name = 'Libras';
+DELETE FROM specialties
+WHERE name IN ('Libras', 'Tradução Simultânea');
 
-UPDATE specialties
-SET name = 'Intérprete de Sinais Internacionais'
-WHERE name = 'Tradução Simultânea';
+INSERT INTO specialties (id, name) VALUES
+('f960f5b0-be40-4bff-8625-84fbe6e86588', 'Intérprete de Libras'),
+('9e9a0cc0-f968-4852-9683-7fa5c138a5da', 'Guia-intérprete de Libras'),
+('6539e352-1742-4f70-a484-c256fc36177a', 'Intérprete Tátil'),
+('f7462efe-b4fe-4098-86ae-d1b0290a32f6', 'Intérprete de Sinais Internacionais');
