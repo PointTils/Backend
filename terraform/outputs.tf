477e877adf57896ada336fb02679fa69d4a9de84
Guilherme Ochoa <guilhermeochoa@gmail.com>
2025-09-29 12:35:30 +0000

    Revert "Corrigir erros no Terraform outputs.tf"
    This reverts commit 8be288323310e38616e88fa54964c5f2e1806e4c.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,22 @@
 output "app_instance_public_ip" {
   description = "IP público da instância EC2 da aplicação"
-  value       = "3.142.18.109"  # IP fixo da instância existente
+  value       = aws_eip.pointtils_eip.public_ip
+  # Garantir que o output é uma string para evitar erros de formatação
+  depends_on  = [aws_eip.pointtils_eip]
 }
 
 output "app_instance_public_dns" {
   description = "DNS público da instância EC2 da aplicação"
-  value       = "ec2-3-142-18-109.us-east-2.compute.amazonaws.com"
+  value       = aws_instance.pointtils_app.public_dns
 }
 
 output "s3_bucket_name" {
   description = "Nome do bucket S3 para testes de API"
-  value       = "pointtils-api-tests"
+  value       = aws_s3_bucket.pointtils_api_tests.bucket
+  depends_on  = [aws_s3_bucket.pointtils_api_tests]
 }
 
 output "ecr_repository_url" {
   description = "URL do repositório ECR"
-  value       = "969285065739.dkr.ecr.us-east-2.amazonaws.com/pointtils"
+  value       = var.create_ecr && length(aws_ecr_repository.pointtils) > 0 ? aws_ecr_repository.pointtils[0].repository_url : "pointtils-repository-already-exists"
 }
