ef88616be07339ae46b68d5ca9d6308c3fd3da8c
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-10-06 20:32:20 -0300

    chore: created mapper to map rating responses

---
+++ pointtils/src/main/java/com/pointtils/pointtils/src/application/mapper/RatingResponseMapper.java
@@ -0,0 +1,33 @@
+package com.pointtils.pointtils.src.application.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.pointtils.pointtils.src.application.dto.responses.RatingResponseDTO;
+import com.pointtils.pointtils.src.application.dto.responses.RatingUserResponseDTO;
+import com.pointtils.pointtils.src.core.domain.entities.Rating;
+import com.pointtils.pointtils.src.core.domain.entities.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RatingResponseMapper {
+    
+    public RatingResponseDTO toResponseDTO(Rating rating, User user) {
+        return RatingResponseDTO.builder()
+            .id(rating.getId())
+            .stars(rating.getStars())
+            .description(rating.getDescription())
+            .appointmentId(rating.getAppointment().getId())
+            .user(toUserResponseDTO(user))
+            .build();
+    }
+
+    private RatingUserResponseDTO toUserResponseDTO(User user) {
+        return RatingUserResponseDTO.builder()
+            .id(user.getId())
+            .name(user.getDisplayName())
+            .picture(user.getPicture())
+            .build();
+    }
+}
