05089928344ec82d90e122c73a6406fa85beba8e
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-10-06 20:31:47 -0300

    chore: created DTOs to map rating requests and responses

---
+++ pointtils/src/main/java/com/pointtils/pointtils/src/application/dto/requests/RatingRequestDTO.java
@@ -0,0 +1,25 @@
+package com.pointtils.pointtils.src.application.dto.requests;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingRequestDTO {
+    
+    private BigDecimal stars;
+    private String description;
+    @JsonProperty("user_id")
+    private UUID userId;
+    @JsonProperty("appointment_id")
+    private UUID appointmentId;
+}
---
+++ pointtils/src/main/java/com/pointtils/pointtils/src/application/dto/responses/RatingResponseDTO.java
@@ -0,0 +1,22 @@
+package com.pointtils.pointtils.src.application.dto.responses;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RatingResponseDTO {
+    private UUID id;
+    private BigDecimal stars;
+    private String description;
+    private UUID appointmentId;
+    private String date;
+    private RatingUserResponseDTO user;
+}
