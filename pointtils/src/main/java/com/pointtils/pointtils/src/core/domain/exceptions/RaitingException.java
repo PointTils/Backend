444443ef93d7ed58970cc0cde40a8815ea663d4c
MateusSNeubarth <mateusneubarth@hotmail.com>
2025-10-06 20:33:39 -0300

    chore: created custom exception handler to throw custom messages

---
+++ pointtils/src/main/java/com/pointtils/pointtils/src/core/domain/exceptions/RaitingException.java
@@ -0,0 +1,7 @@
+package com.pointtils.pointtils.src.core.domain.exceptions;
+
+public class RaitingException extends RuntimeException {
+    public RaitingException(String message) {
+        super(message);
+    }
+}
