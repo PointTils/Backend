b2e453b0e50ad1d8ffacb578bd727cfb19870c3e
MatheusBerwaldt <mathsmachado18@gmail.com>
2025-10-17 18:32:26 -0300

    fix: imports

--- pointtils/src/test/java/com/pointtils/pointtils/src/application/controllers/ParametersControllerTest.java
+++ pointtils/src/test/java/com/pointtils/pointtils/src/application/controllers/ParametersControllerTest.java
@@ -1,36 +1,46 @@
 package com.pointtils.pointtils.src.application.controllers;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pointtils.pointtils.src.application.dto.requests.ParametersBasicRequestDTO;
+import com.pointtils.pointtils.src.application.dto.requests.ParametersPatchRequestDTO;
 import com.pointtils.pointtils.src.application.dto.responses.ParametersResponseDTO;
 import com.pointtils.pointtils.src.application.exceptions.GlobalExceptionHandler;
 import com.pointtils.pointtils.src.application.services.ParametersService;
 import com.pointtils.pointtils.src.domain.entities.Parameters;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.mockito.MockitoAnnotations;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
 import java.util.UUID;
 
-import static com.pointtils.pointtils.src.util.TestDataUtil.createPersonCreationRequest;
-import static com.pointtils.pointtils.src.util.TestDataUtil.createPersonPatchRequest;
-import static com.pointtils.pointtils.src.util.TestDataUtil.createPersonResponse;
-import static com.pointtils.pointtils.src.util.TestDataUtil.createPersonUpdateRequest;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
-@EnableAutoConfiguration(exclude = S3AutoConfiguration.class)
 @AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 class ParametersControllerTest {
